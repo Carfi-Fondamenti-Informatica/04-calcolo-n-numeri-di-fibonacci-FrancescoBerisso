3dbda179abd8478ff4f1c3b08dc06cd9165460b8
FrancescoBerisso <115414198+FrancescoBerisso@users.noreply.github.com>
2022-11-23 11:27:42 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,18 @@
 using namespace std;
 
 int main() {
-   return 0;
+    int n1 = 1, n2 = 1, n3 = 0, cifre = 0, i = 3;
+    cin >> cifre;
+    if(cifre >= 2){
+        cout << n1 << " " << n2 << " ";
+        for(; i <= cifre; i++){
+            n3 = n1 + n2;
+            n1 = n2;
+            n2 = n3;
+            cout << n3 << " ";
+        }
+    else{
+        cout << "errore" << endl;
+    } 
+    return 0;
 }
